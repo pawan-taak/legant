7dfa567d28f431c7cc2a490d6c717d71fc20d263
pawan-taak <pawantaak786@gamil.com>
2024-03-05 15:58:23 +0530

    folder section

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+function Header() {
+    return (
+        <>
+
+        // header section
+
+
+            <div className="background_header">
+                <div className="container">
+                    <img src={require('../assest/img/header_img/ticket-percent.png')} alt="" />
+                    <p className='h_sm_heading'>30% off storewide — Limited time! </p>
+                    <p className='h_sm_heading'>Shop Now</p>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+export default Header
