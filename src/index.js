e897d220026f4581f247eb0f77bf3c30faaf8bc8
pawan-taak <pawantaak786@gamil.com>
2024-03-05 19:42:21 +0530

    hero section

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/header';
+import Footer from './components/footer';
+import HeroSection from './Home_section/hero_section';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <HeroSection/>
+    <Footer />
   </React.StrictMode>
 );
 
