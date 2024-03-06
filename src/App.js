a4af1f503cce904f5a7eb6839969d4cf07b03ac7
pawan-taak <pawantaak786@gamil.com>
2024-03-06 18:22:18 +0530

    hero section update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/page/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes,useLocation } from 'react-router-dom';
 import SignIn from './components/page/signin';
 import SignUp from './components/page/signup';
 
@@ -12,13 +12,14 @@ function App(){
     <>
 
 <BrowserRouter>
-
+{window.location.pathname !=='/signin' && window.location.pathname !=='/signup' && <Header/>}
 <Routes>
   <Route element={<Home/>} path="/" />
   <Route element={<SignIn/>} path="/signin" />
   <Route element={<SignUp/>} path="/signup" />
 
 </Routes>
+{window.location.pathname !=='/signin' && window.location.pathname !=='/signup' && <Footer/>}
 </BrowserRouter>
     </>
   );
