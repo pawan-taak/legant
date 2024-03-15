f57995b4c77aaeb3f1916cfa7f1240a504217db6
shishpal-nokhwal <nokhwalshishpal29@gmail.com>
2024-03-15 10:55:06 +0530

    signup-change

---
+++ src/components/page/complete.jsx
@@ -0,0 +1,28 @@
+function Complete(){
+    return(
+        <>
+        <div className="container">
+        <div className="complete">
+                        <h2 className="hero_secondary check_out_heading">Complete!</h2>
+                        <div className=" complete_order_through">
+                            <div className=" complete_order_details">
+                                <div className=" complete_checked">1</div>
+                                <div className="shipping">Shipping cart</div>
+                            </div>
+                            <div className="order_details">
+                                <div className=" complete_checkeout_deatils">2</div>
+                                <div className=" complete_check_out_info">Checkout details</div>
+                            </div>
+                            
+                            <div className="order_details">
+                                <div className="complete_order_info">3</div>
+                                <div className="complete_order">Order complete</div>
+                            </div>
+                        </div>
+                    </div>
+            <div></div>
+        </div>
+        </>
+    )
+}
+export default Complete
